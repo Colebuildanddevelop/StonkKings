d8e7cc3563f436cc3efeb600e6d67f3beabc21c6
colebuildanddevelop <colebuildanddevelop@gmail.com>
2020-08-03 21:44:51 -0500

    server only

--- client/src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-// import reducers here
-import auth from "./auth.reducers";
-import tournament from "./tournament.reducers";
-import entry from "./entry.reducers";
-import trade from "./trade.reducers";
-
-export default combineReducers({ auth, tournament, entry, trade });
-
