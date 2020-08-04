9ab7c0dc998e8edfe1319b00eeaedda48321acb5
colebuildanddevelop <colebuildanddevelop@gmail.com>
2020-08-03 21:34:01 -0500

    adding build

---
+++ server/build/precache-manifest.30b085a8e70352511003161aa5c82872.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efba5f38a83dd9acae26b82fff22db60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e269137e07898711fd7d",
+    "url": "/static/js/2.53d22d0f.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.53d22d0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93bc6082ae4024878e88",
+    "url": "/static/js/main.771cc602.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
---
+++ server/build/precache-manifest.d231cffdd955bfcf83e25515b11bafac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d3e848e9166b9a2d07d295bcc52418d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e269137e07898711fd7d",
+    "url": "/static/js/2.53d22d0f.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.53d22d0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4746d9d5fb07db4fb79c",
+    "url": "/static/js/main.13d9374e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
